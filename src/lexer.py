6642a7c5585ec2c258051b5d04cb2b9595e7977c
Liam Wilson <liamw5265@gmail.com>
2026-09-24 14:41:58 -0700

    Finished lexer file, final output is a token and lexeme table

--- src/lexer.py
+++ src/lexer.py
@@ -22,13 +22,12 @@ starting_state = '1'
 
 operator_list = ['+', '-', '*', '/', '<', '>', '<=', '>=', '=', '==', '!=']
 
-keywords_list = ['integer', 'boolean', 'real', 'if', 'else', 'fi', 'while', 'return', 'get', 'put', 'true', 'false', 'function']
+keywords_list = ['integer', 'boolean', 'real', 'if', 'else', 'fi', 'while', 
+                 'return', 'get', 'put', 'true', 'false', 'function']
 
-separator_list = ['(', ')', '{', '}', ' ', '@', ';', ',', ':']
+separator_list = ['(', ')', '{', '}', ' ', '@', ';', ',', ':', '\n', "'", '"']
 
 comment = '!'
-
-
 
 def convert_input_to_fsm_input(input_str: str) -> str:
     '''
@@ -61,10 +60,10 @@ def convert_input_to_fsm_input(input_str: str) -> str:
     return result
 
 def fsm(input_str: str,
-                   starting_states: str,
-                   accepting_states: list[str],
-                   transition_function: dict[str, dict[str, str]]
-                    ) -> bool:
+        starting_states: str,
+        accepting_states: list[str],
+        transition_function: dict[str, dict[str, str]]
+        ) -> bool:
     '''
     Checks [input_str] if it is a valid token
 
@@ -74,15 +73,13 @@ def fsm(input_str: str,
         - accepting_states:
         - transition_function:
 
-    returns (bool): 
+    returns (bool): True or False if [input_str] is a valid token relative to 
+                    the transition functions passed
     '''
 
     new_input_str = convert_input_to_fsm_input(input_str)
 
     current_state = starting_states
-
-    # Testing
-    #print(new_input_str)
 
     for input in str(new_input_str):
         try:
@@ -102,11 +99,8 @@ def read_file(file_path: str) -> str:
 
     with open(file_path, 'r', encoding = 'utf-8') as file:
         file_contents = file.read()
-        print(file_contents)
 
     input_without_comments = remove_comments(file_contents)
-
-    print(input_without_comments)
 
     return input_without_comments
 
@@ -121,9 +115,13 @@ def remove_comments(text_input: str) -> str:
         current_char = text_input[index]
         next_char = text_input[index + 1]
 
-        if current_char == comment and not inside_comment and next_char != '=':
+        if ((current_char == comment) and 
+            (not inside_comment) and 
+            (next_char != '=')):
             inside_comment = True
-        elif current_char == comment and inside_comment and next_char != '=':
+        elif ((current_char == comment) and 
+              (inside_comment) and 
+              (next_char != '=')):
             inside_comment = False
         elif not inside_comment:
             input_without_comments += current_char
@@ -136,10 +134,8 @@ def separate_tokens(input_str: str) -> list[(str, str)]:
     tokens = []
     token = ''
 
-    input_str = 'while (fahr <= upper) a = 23.00; '
-    for index in range(len(input_str) - 1):
+    for index in range(len(input_str)):
         current_char = input_str[index]
-        #next_char = input_str[index + 1]
 
         if ((current_char in separator_list) or 
             (current_char in operator_list) or 
@@ -150,13 +146,65 @@ def separate_tokens(input_str: str) -> list[(str, str)]:
 
         token += current_char
 
-    print(tokens)
+    with_double_op_tokens_list = []
+    is_double_op = False
+
+    for index in range(len(tokens)):
+        if is_double_op:
+            is_double_op = False
+            continue
+
+        if ((tokens[index] in operator_list) and 
+            (tokens[index + 1] in operator_list)):
+            double_op = tokens[index] + tokens[index + 1]
+            with_double_op_tokens_list.append(double_op)   
+            is_double_op = True     
+        else:
+            with_double_op_tokens_list.append(tokens[index])
+
+    final_token_list = []
+    for token in with_double_op_tokens_list:
+        if token != ' ' and token != '\n':
+            final_token_list.append(token)
+    print(final_token_list)
+    return final_token_list
+
+def print_token_and_lexeme(lexeme_list: list[str]):
+    '''
+    '''
+    token = ''
+    print('Token         Lexem\n'
+          '-------------------')
+    for lexeme in lexeme_list:
+        if lexeme in operator_list:
+            token = 'Operator  '
+        elif lexeme in separator_list:
+            token = 'Separator '
+        elif lexeme in keywords_list:
+            token = 'Keyword   '
+        elif fsm(lexeme,
+                 starting_state,
+                 integer_accepting_states,
+                 integer_transition_function):
+            token = 'Integer   '
+        elif fsm(lexeme,
+                 starting_state,
+                 real_accepting_states,
+                 real_transition_function):
+            token = 'Real      '
+        elif fsm(lexeme,
+                 starting_state,
+                 identifier_accepting_states,
+                 identifier_transition_function):
+            token = 'Identifier'
+        print(f'{token}    {lexeme}')
+        
         
 
 
 
 
 if __name__ == '__main__':
-    #no_comment_text = read_file('tests/test.txt')
-
-    separate_tokens('')
+    no_comment_text = read_file('tests/test_data/test.txt')
+    lexeme_list = separate_tokens(no_comment_text)
+    print_token_and_lexeme(lexeme_list)
